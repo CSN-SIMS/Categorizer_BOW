076b9d47477f459463091c75622a2cc3b28e0adb
timbolo6 <tim.wywiol.i17@nordakademie.org>
2019-10-10 22:46:43 +0200

    add comparing Arrays into a vector Todo: - constant order of arrayofSigWordsTotal - fixing array into vector

--- BagOfWords.py
+++ BagOfWords.py
@@ -12,16 +12,41 @@ language = 'english'
 # (brown.words()) #testing nltk
 
 directory = './inputEmails'
+
+
 def loadingDirectory(directory):
     documents = ""
+    counterOfEmails = 0
     if not path.exists(directory):
         print('no folder exists')
     for filename in listdir(directory):
         pathToFile = directory + '/' + filename
-        email= open(pathToFile, 'r', encoding='utf-8')
-        documents = documents + " " + str(email.read())
+        email = open(pathToFile, 'r', encoding='utf-8')
+        document = str(email.read())
+        printSignificantWordOfFile(document)
+        documents = documents + " " + document
         email.close()
+        counterOfEmails = counterOfEmails + 1
+    print('Amount of Emails: ' + str(counterOfEmails))
     return documents
+
+
+def loadingFilesIntoArray(directory):
+    arrayofEmails = []
+    if not path.exists(directory):
+        print('no folder exists')
+    for filename in listdir(directory):
+        pathToFile = directory + '/' + filename
+        email = open(pathToFile, 'r', encoding='utf-8')
+        document = str(email.read())
+        arrayofEmails.append(printSignificantWordOfFile(document))
+        email.close()
+    print(arrayofEmails)
+    return arrayofEmails
+
+
+def printSignificantWordOfFile(filecontent):
+    return stemming(removeSymbols(removeStopwords(tokenize(filecontent))))
 
 
 def tokenize(textfile):
@@ -43,8 +68,9 @@ def removeSymbols(arrayOfStrings):
     new_word_list = arrayOfStrings
     for index in arrayOfStrings:
         if index in list_of_removing_symbols:
-           new_word_list.remove(index)
+            new_word_list.remove(index)
     return new_word_list
+
 
 def stemming(arrayOfStrings):
     new_word_list = []
@@ -59,9 +85,26 @@ def removeDuplicates(arraOfStrings):
     return new_word_list
 
 
-# def printSignificantWords
+def printSignificantWordsofDirectory(directoryPath):
+    print(removeDuplicates(stemming(removeSymbols(removeStopwords(tokenize(loadingDirectory(directoryPath)))))))
+    return removeDuplicates(stemming(removeSymbols(removeStopwords(tokenize(loadingDirectory(directoryPath))))))
 
- #def mergeArrays():
+def countOccurencyOfWordsPerEmail(directory):
+    vectorMatrix = []
+    arrayOfSigWordsTotal = printSignificantWordsofDirectory(directory)
+    arrayOfSigWordsFile = loadingFilesIntoArray(directory)
+    for array in arrayOfSigWordsFile:
+        for word in array:
+            for wordtotal in arrayOfSigWordsTotal:
+                if word == wordtotal:
+                    vectorMatrix.append(1)
+                else:
+                    vectorMatrix.append(0)
+    print(vectorMatrix)
+    return vectorMatrix
 
-print(removeDuplicates(stemming(removeSymbols(removeStopwords(tokenize(loadingDirectory(directory)))))))
+
+
+countOccurencyOfWordsPerEmail(directory)
+
 
